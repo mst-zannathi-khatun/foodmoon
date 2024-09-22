d25f1e92650e56cb6586c4afbfff9bfe73e984fb
zannathi-khatun <m.zannathi.k@gmail.com>
2024-09-22 10:08:12 +0530

    Home Screen Shop by Category Section Part UI Design Completed

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -3,5 +3,6 @@ import 'dart:ui';
 class AppColors{
   static Color pinkShadow= const Color(0xffff008d);
   static Color pinkLightShadow= const Color(0xfffbbae9);
+  static Color lightYellow= const Color(0xffFFF9C4);
   static Color lightGrey= const Color(0xffcad7da);
 }
--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -7,6 +7,7 @@ import 'components/homescreencomponents/filterbuttonssection/filterbuttonssectio
 import 'components/homescreencomponents/promotionalslides/homescreenpromotionalslides.dart';
 import 'components/homescreencomponents/recommended_order_section/recommended_order.dart';
 import 'components/homescreencomponents/searchbarcontainer/searchbarcontainer.dart';
+import 'components/homescreencomponents/shopcategorysection/shopcategorysection.dart';
 import 'components/homescreencomponents/topshopssection/topshopssection.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -28,8 +29,10 @@ class _HomeScreenState extends State<HomeScreen> {
           children: [
             /// Search Bar Section
             HomeScreenSearchBar(),
+
             /// Container Search Bar Section
             SearchBarContainer(),
+
             /// FilterButtonsSections
             SizedBox(
               height: 16,
@@ -38,6 +41,7 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(
               height: 16,
             ),
+
             /// Oder it again sections
             HomeScreenRecommendedOrder(),
             SizedBox(
@@ -46,12 +50,16 @@ class _HomeScreenState extends State<HomeScreen> {
             HomeScreenPromotionalSlides(),
             //// Cuisine Item Section
             CuisineItemSection(),
+
             /// Top Shops Sections
             TopShopsSection(),
+            /// shop by category
+            ShopCategorySection(),
           ],
         ),
       ),
     );
   }
 }
+
 
